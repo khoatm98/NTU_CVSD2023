32f2ea2ecdf036547f3b2b9627f9e6bafa231f29
khoatm98 <khoatm98.com>
2024-07-23 14:29:43 +0800

    clean up

--- HW3/01_RTL/csrc/filelist.cu
+++ HW3/01_RTL/csrc/filelist.cu
@@ -4,16 +4,16 @@ ifeq ($(LDVERSION), 0)
 endif
 
 ARCHIVE_OBJS=
-ARCHIVE_OBJS += _84524_archive_1.so
-_84524_archive_1.so : archive.160/_84524_archive_1.a
+ARCHIVE_OBJS += _86883_archive_1.so
+_86883_archive_1.so : archive.161/_86883_archive_1.a
 	@$(AR) -s $<
-	@$(PIC_LD) -shared  -Bsymbolic $(LD_NORELAX_FLAG)  -o .//../simv.daidir//_84524_archive_1.so --whole-archive $< --no-whole-archive
+	@$(PIC_LD) -shared  -Bsymbolic $(LD_NORELAX_FLAG)  -o .//../simv.daidir//_86883_archive_1.so --whole-archive $< --no-whole-archive
 	@rm -f $@
-	@ln -sf .//../simv.daidir//_84524_archive_1.so $@
+	@ln -sf .//../simv.daidir//_86883_archive_1.so $@
 
 
 ARCHIVE_OBJS += _prev_archive_1.so
-_prev_archive_1.so : archive.160/_prev_archive_1.a
+_prev_archive_1.so : archive.161/_prev_archive_1.a
 	@$(AR) -s $<
 	@$(PIC_LD) -shared  -Bsymbolic $(LD_NORELAX_FLAG)  -o .//../simv.daidir//_prev_archive_1.so --whole-archive $< --no-whole-archive
 	@rm -f $@
